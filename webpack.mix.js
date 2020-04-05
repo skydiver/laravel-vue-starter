1108e1a702680ae8269940baa7683d75b3263d74
Martín M <skydiver@users.noreply.github.com>
2020-04-04 22:36:52 -0300

    installed tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,7 +14,11 @@ const mix = require('laravel-mix');
 
 mix
   .js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css')
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss('./tailwind.config.js')]
+  });
 
 mix.copy('resources/assets/images', 'public/images', false);
 
